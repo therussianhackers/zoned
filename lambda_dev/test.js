6c569d70ee2cefecc6c5e04cdf5c5ce8010fd967
Ron Tsui <ronsuij@gmail.com>
2018-11-03 15:07:33 -0700

    (feat) continues tests with lambda; about to begin accessing clarifai

--- lambda_dev/test.js
+++ lambda_dev/test.js
@@ -1,3 +1,16 @@
-exports.handler = function(blah) {
-  console.log('blah');
-}
+require('dotenv').config();
+let clarafai_api_key = process.env.CLARAFAI_API_KEY;
+
+exports.handler = function(event, context, callback) {
+  console.log('THE KEY: ');
+  console.log(clarafai_api_key);
+
+  console.log(event);
+
+  callback(null, {
+    statusCode: 200,
+    body: "blah"
+  })
+
+
+};
